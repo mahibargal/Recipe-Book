2c6617218a59cd78af645d9e6571321fd5740516
mahibargal <maheshb@easyecom.io>
2024-12-03 12:47:40 +0530

    authentication login autologin logout autologout functionality added with authguard.

--- recipe-project/src/app/shared/recepe-storage.service.ts
+++ recipe-project/src/app/shared/recepe-storage.service.ts
@@ -1,37 +1,52 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
-import{map} from 'rxjs/operators'
+import { exhaustMap, map, take, tap } from 'rxjs/operators'
+import { AuthService } from "../auth/auth.service";
 
-@Injectable({providedIn:'root'})
-export class DataStorageService{
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
 
-    constructor(private recipeSrvc:RecipeService,private http:HttpClient){
+    constructor(
+        private recipeSrvc: RecipeService,
+        private http: HttpClient,
+        private authSrvc: AuthService
+    ) {
 
     }
 
     saveRecipeData() {
         const recipes: Recipe[] = this.recipeSrvc.getRecipe();
 
-        this.http.put('https://code-69c0d-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe((res) => {
+        return this.http.put('https://code-69c0d-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe((res) => {
             console.log(res);
         })
     }
 
-    fetchRecipeData(){
-        this.http.get<Recipe[]>('https://code-69c0d-default-rtdb.firebaseio.com/recipes.json')
-        .pipe(map (respnData=>{
-            return respnData.map((recipe)=>{
-                return { ...recipe , ingredients:recipe?.ingredients ?recipe?.ingredients : [] }
+    fetchRecipeData() {
+        return this.authSrvc.currentUser.pipe(
+            take(1),
+            exhaustMap(user => {
+                const token = user?.getToken;
+                return this.http.get<Recipe[]>(
+                    'https://code-69c0d-default-rtdb.firebaseio.com/recipes.json',
+                    {params: new HttpParams().set('auth', token)}
+                )
+            }),
+            map(respnData => {
+                return respnData.map((recipe) => {
+                    return { ...recipe, ingredients: recipe?.ingredients ? recipe?.ingredients : [] }
+                })
             })
-        
-        })
-        )
-        .subscribe((res) => {
+            , tap((res: any) => {
+                this.recipeSrvc.assignFetchRecipe(res)
+                console.log(res);
+            })
+        ).subscribe((res) => {
             this.recipeSrvc.assignFetchRecipe(res)
             console.log(res);
-        }) 
+        })
     }
 }
